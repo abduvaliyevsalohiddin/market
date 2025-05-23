2a0c4ce94da97c7bc92fe2c9a2b01d27e08f143a
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2025-05-23 23:02:23 +0500

    //.

--- user/models.py
+++ user/models.py
@@ -20,6 +20,7 @@ class Profile(AbstractUser):
     profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
     address = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(blank=True)
+    card = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return f"{self.phone}"
