6f2d7bb26d7309a5086976904c7a13afa89657ac
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2025-06-10 14:39:57 +0500

    user_firm

--- user/models.py
+++ user/models.py
@@ -15,12 +15,15 @@ ROLE_CHOICES = (
 class Profile(AbstractUser):
     phone = models.CharField(max_length=15, blank=True)
     birth_date = models.DateField(blank=True, null=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
     address = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(blank=True)
     card = models.CharField(max_length=20, blank=True, null=True)
+
+    firm_name = models.CharField(max_length=255, blank=True, null=True)
+    firm_file = models.FileField(upload_to='firm_file', blank=True, null=True)
 
     def __str__(self):
         return f"{self.phone}"
